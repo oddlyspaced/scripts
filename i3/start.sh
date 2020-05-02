9ad10408224536aaa92ed44c8a4f08cb5e3a33cd
oddlyspaced <oddlyspaced1@gmail.com>
2020-05-02 10:45:06 +0530

    scripts: Remove polybar and i3 scripts

--- i3/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to run commands on i3 start
-nitrogen --restore
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-sleep 1 &
- compton -b &
-~/.scripts/i3/refesh.sh &
-exit 0
